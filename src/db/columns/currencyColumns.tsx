2d28ee2b0f94d50849510213f14ccd9c70694d09
Osman.ordu <orduosmann@gmail.com>
2025-12-27 19:13:57 +0300

    main | Clean currencyColumn

--- src/db/columns/currencyColumns.tsx
+++ src/db/columns/currencyColumns.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Column } from '@/components/ui/custom-grid';
 import { AnimatedPrice } from '@/feautures/market/components/AnimatedPrice';
-import { ThemedText } from '@/components/ui/themed-text';
 import { styles } from '@/feautures/market/components/MarketList/styles';
 
 export const currencyColumns: Column[] = [
@@ -29,36 +28,4 @@ export const currencyColumns: Column[] = [
   },
 ];
 
-export const quickTransactionColumns: Column[] = [
-  {
-    dataField: 'baseAsset',
-    caption: 'Döviz',
-    addition: {
-      align: 'left',
-    },
-  },
-  {
-    dataField: 'amount',
-    caption: 'Miktar',
-    addition: {
-      align: 'center',
-    },
-  },
-  
-  {
-    dataField: 'total',
-    caption: 'Türk Lirası',
-    addition: {
-      align: 'center',
-      renderCell: (value: number) => (
-        <ThemedText style={styles.resultAmount}>
-          {value.toLocaleString('tr-TR', {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2,
-          })}
-        </ThemedText>
-      ),
-    },
-  },
-];
 
